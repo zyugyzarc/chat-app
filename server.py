e4d92009d893bf7d3693206f2f66bd1bb2ce9e2c
zyugyzarc <zyugyzarc@gmail.com>
2024-06-16 12:21:50 -0700

    add secret server

---
+++ server.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/secret.js')
+def keys():
+    return ( ""
+        + "   /* Please dont steal my keys */  "
+        + "   const sconf = {"
+        + "      host: " + repr(os.environ['KV_REST_API_URL']) + ','
+        + "      token: " + repr(os.environ['KV_REST_API_TOKEN'])
+        + "}"
+    )
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'app.html')
+
+@app.route('/<path:path>')
+def file(path):
+    return send_from_directory('.', path)
+
+if __name__ == '__main__':
+    app.run()
